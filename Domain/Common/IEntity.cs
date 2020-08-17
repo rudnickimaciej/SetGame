ec85c2405f1aef1b939b45e0e4a3637e08f0874b
Maciej Rudnicki <mrrudnicki@wp.pl>
2020-08-17 14:54:13 +0200

    Add project files.

---
+++ Domain/Common/IEntity.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Domain.Common
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Domain/Games/Game.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Games
+{
+    public class Game : IEntity
+    {
+        public int Id { get; set; }
+        
+    }
+}
